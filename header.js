3decf6f2bd9b3c55a959295a3316b4de7ad987c5
Melanie Lecomte <melacrea@gmail.com>
2017-11-17 11:26:33 +0100

    header react

---
+++ header.js
@@ -0,0 +1,13 @@
+var Header = React.createClass({
+
+  render: function() {
+    return (
+      <div>
+          <p>Mon header react</p>
+      </div>
+    );
+  }
+});
+
+
+ReactDOM.render(<Header />, document.getElementById('header'));
--- header.php
+++ header.php
@@ -2,30 +2,30 @@
 /*
 Plugin Name: header
 Plugin URI: https://www.advancedcustomfields.com/
-Description: Customise WordPress with powerful, professional and intuitive fields.
-Version: 1.0.2
+Description: Header en react
+Version: 1.0.4
 Author: Melanie Lecomte
 */
 
-add_shortcode( 'wpshout_react_quiz', 'wpshout_react_quiz_show_quiz' );
-function wpshout_react_quiz_show_quiz() {
-	return '<div id="quiz"></div>';
+add_shortcode( 'wpshout_react_header', 'wpshout_react_show_header' );
+function wpshout_react_show_header() {
+	return '<div id="header"></div>';
 }
 
-add_action( 'wp_enqueue_scripts', 'wpshout_react_quiz_enqueue_scripts' );
-function wpshout_react_quiz_enqueue_scripts() {
+add_action( 'wp_enqueue_scripts', 'wpshout_react_header_enqueue_scripts' );
+function wpshout_react_header_enqueue_scripts() {
 
 	wp_enqueue_script( 'react', plugin_dir_url( __FILE__ ) . 'react/build/react.min.js' );
 	wp_enqueue_script( 'react-dom', plugin_dir_url( __FILE__ ) . 'react/build/react-dom.min.js' );
 	wp_enqueue_script( 'babel', 'https://npmcdn.com/babel-core@5.8.38/browser.min.js', '', null, false );
-	wp_enqueue_script( 'wpshout-react-quiz', plugin_dir_url( __FILE__ ) . 'wpshout-react-quiz.js' );
-	wp_enqueue_style( 'wpshout-react-quiz', plugin_dir_url( __FILE__ ) . 'wpshout-react-quiz.css' );
+	wp_enqueue_script( 'wpshout-react-header', plugin_dir_url( __FILE__ ) . 'header.js' );
+	wp_enqueue_style( 'wpshout-react-header', plugin_dir_url( __FILE__ ) . 'header.css' );
 }
 
-// Add "babel" type to quiz script
-add_filter( 'script_loader_tag', 'wpshout_react_quiz_add_babel_type', 10, 3 );
-function wpshout_react_quiz_add_babel_type( $tag, $handle, $src ) {
-	if ( $handle !== 'wpshout-react-quiz' ) {
+// Add "babel" type to header script
+add_filter( 'script_loader_tag', 'wpshout_react_header_add_babel_type', 10, 3 );
+function wpshout_react_header_add_babel_type( $tag, $handle, $src ) {
+	if ( $handle !== 'wpshout-react-header' ) {
 		return $tag;
 	}
 
