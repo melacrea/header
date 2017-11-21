7658382b13b7057279adef59ccb32c52b3c79eb2
Melanie Lecomte <melacrea@gmail.com>
2017-11-21 15:28:46 +0100

    clean plugin

--- header.php
+++ header.php
@@ -3,13 +3,13 @@
 Plugin Name: header
 Plugin URI: https://www.advancedcustomfields.com/
 Description: Header en react
-Version: 2.0.0
+Version: 2.0.1
 Author: Melanie Lecomte
 */
 
 add_shortcode( 'wpshout_react_header', 'wpshout_react_show_header' );
 function wpshout_react_show_header() {
-	return '<div id="root"></div>';
+	return '<div id="header"></div>';
 }
 
 add_action( 'wp_enqueue_scripts', 'wpshout_react_header_enqueue_scripts' );
@@ -18,8 +18,8 @@ function wpshout_react_header_enqueue_scripts() {
 	//wp_enqueue_script( 'react', plugin_dir_url( __FILE__ ) . 'react/build/react.min.js' );
 	//wp_enqueue_script( 'react-dom', plugin_dir_url( __FILE__ ) . 'react/build/react-dom.min.js' );
 	wp_enqueue_script( 'babel', 'https://npmcdn.com/babel-core@5.8.38/browser.min.js', '', null, false );
-	wp_enqueue_script( 'wpshout-react-header', plugin_dir_url( __FILE__ ) . 'react/shopping-list/static/js/main.c4ddab5e.js' );
-	wp_enqueue_style( 'wpshout-react-header', plugin_dir_url( __FILE__ ) . 'react/shopping-list/static/css/main.279176be.css' );
+	wp_enqueue_script( 'wpshout-react-header', plugin_dir_url( __FILE__ ) . 'react/build/static/js/main.9d742845.js' );
+	wp_enqueue_style( 'wpshout-react-header', plugin_dir_url( __FILE__ ) . 'react/build/static/css/main.279176be.css' );
 }
 
 // Add "babel" type to header script
